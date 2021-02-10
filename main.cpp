65f343b26ca4b6f1364833508e70334d494343d7
Krimika Keemtee <krimikakeemtee@gmail.com>
2021-02-10 21:28:10 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+  string greeting = "Hello World!\n";
+  cout << greeting << endl;
+  cout << "I love C++ \n";
+  return 0;
+}
